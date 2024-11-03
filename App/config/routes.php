5e35b1600752c484aa50cb44ff2f739193f2bc5c
HENRIQUE CLARANHAN DE OLIVEIRA <henrique.claranhan@ufms.br>
2024-11-03 14:35:53 -0400

    Redireciona rota raiz para listagem de produtos

--- App/config/routes.php
+++ App/config/routes.php
@@ -57,7 +57,7 @@ return function (RouteBuilder $routes): void {
          * its action called 'display', and we pass a param to select the view file
          * to use (in this case, templates/Pages/home.php)...
          */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+        $builder->connect('/', ['controller' => 'Produtos', 'action' => 'index']);
 
         $builder->connect('/login', ['controller' => 'Users', 'action' => 'login', 'login']);
 
