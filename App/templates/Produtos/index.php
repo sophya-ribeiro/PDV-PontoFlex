bbcac4680a9fe865bc0443ce3839562ed230eb4e
HENRIQUE CLARANHAN DE OLIVEIRA <henrique.claranhan@ufms.br>
2024-11-11 11:12:20 -0400

    Componentiza footer

--- App/templates/Produtos/index.php
+++ App/templates/Produtos/index.php
@@ -28,15 +28,7 @@
 		</nav>
 	</div>
 
-	<footer>
-		<a class="" href="#">Termos e condições</a>
-		<a class="" href="#">Suporte</a>
-
-		<div>
-			© 2024 PontoFlex. <br>
-			Todos direitos reservados
-		</div>
-	</footer>
+	<?= $this->element('default/footer'); ?>
 </div>
 
 <section class="container-central">
---
+++ App/templates/element/default/footer.php
@@ -0,0 +1,9 @@
+<footer>
+	<a class="" href="#">Termos e condições</a>
+	<a class="" href="#">Suporte</a>
+
+	<div>
+		© 2024 PontoFlex. <br>
+		Todos direitos reservados
+	</div>
+</footer>
