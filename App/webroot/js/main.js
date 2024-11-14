07a6ec9fcc25eff0c338cd8ac3e8ed68b4e9b468
HENRIQUE CLARANHAN DE OLIVEIRA <henrique.claranhan@ufms.br>
2024-11-14 17:15:33 -0400

    Fecha notificações após 5 segundos

--- App/webroot/js/main.js
+++ App/webroot/js/main.js
@@ -1,15 +1,33 @@
-function openModalWithHash(hash) {
+function openModalWithHash() {
+	const hash = window.location.hash;
+
+	if (!window.location.hash.startsWith("#modal")) {
+		return;
+	}
+
 	if (!document.querySelector(hash)) {
 		return;
 	}
 
-	const modal = new bootstrap.Modal(hash);
+	const bsModal = new bootstrap.Modal(hash);
 
-	modal.show();
+	bsModal.show();
+}
+
+function hideToasts() {
+	const toasts = document.querySelectorAll(".toast");
+
+	toasts.forEach((toast) => {
+		const bsToast = new bootstrap.Toast(toast);
+
+		setTimeout(() => {
+			bsToast.hide();
+		}, 5000);
+	});
 }
 
 window.onload = () => {
-	if (window.location.hash && window.location.hash.startsWith("#modal")) {
-		openModalWithHash(window.location.hash);
-	}
+	openModalWithHash();
+
+	hideToasts();
 }
